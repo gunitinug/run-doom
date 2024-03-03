d52e5ae298475a6b2f15607cad300bf1048f999d
Logan Won-Ki Lee <logan.wonki.lee@gmail.com>
2024-03-04 10:42:37 +1100

    run doom file added

---
+++ run-doom.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Run prboom-plus with an wad file.
+
+# Get choice number.
+ls /my-downloads/Wads	# custimise path to your wads files
+read -p "Enter your choice: " choice; echo choice: $choice
+upper="$(ls /my-downloads/Wads | wc -l)"
+regex="^[1-$upper]$"
+
+# Run the number or quit with error message.
+if [[ "$choice" =~ $regex ]]; then
+	echo chosen choice: $choice 
+	prboom-plus -iwad /my-downloads/Wads/"$(ls /my-downloads/Wads | awk -v c="$choice" 'NR==c')"
+else
+	echo "wrong number!"
+fi
